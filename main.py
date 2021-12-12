435d395a810ae3377ff2dd32b9b10cad0a4f7b24
Krzx <81083343+Krzx1337@users.noreply.github.com>
2021-12-12 05:19:14 +0000

    :weary:

---
+++ main.py
@@ -0,0 +1,11 @@
+import threading, requests
+
+url = input(f"Link: ")
+
+def send():
+    r = requests.get(url)
+    
+if __name__ == "__main__":
+    for i in range(69696966969):
+        threading.Thread(target=send).start()
+        print("%s Views sent."% i)
